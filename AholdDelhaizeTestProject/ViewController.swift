1bbcf4b2050c65f3b36d70ef22a3d95078735798
Sos Avetyan <avetyansos@gmail.com>
2022-08-17 09:06:27 +0400

    Initial Commit

---
+++ AholdDelhaizeTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AholdDelhaizeTestProject
+//
+//  Created by Sos Avetyan on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
